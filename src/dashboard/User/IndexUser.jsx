2d79a4ffe0ef76ab5587220f47c1e868be8108e7
Alvinda_akbar10 <89963240+Nandaakbar15@users.noreply.github.com>
2025-09-28 11:32:30 +0700

    Fix error ga bisa nambah data dan update data.

--- src/dashboard/User/IndexUser.jsx
+++ src/dashboard/User/IndexUser.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import NavBar from "../../components/Navbar";
-import {Card} from '../../components/ui/card';
+import { Card } from "../../components/ui/card";
 import {
   Table,
   TableBody,
@@ -8,104 +8,137 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "../../components/ui/table"
+} from "../../components/ui/table";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Modal from "@/components/Modal";
 
 export default function IndexUserPages() {
-    const [users, setUsers] = useState([]);
-    const [message, setMessage] = useState("");
-    const [showModal, setShowModal] = useState(false);
-    const navigate = useNavigate();
+  const [users, setUsers] = useState([]);
+  const [message, setMessage] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
 
-    const getUsers = async() => {
-        try {
-            const response = await axios.get("https://usermanagement-api.vercel.app/api/users");
-            setUsers(response.data.data);
-        } catch(error) {
-            console.error("Error : ", error);
-        }
+  const getUsers = async () => {
+    try {
+      const response = await axios.get(
+        "https://usermanagement-api.vercel.app/api/users"
+      );
+      setUsers(response.data.data);
+    } catch (error) {
+      console.error("Error : ", error);
     }
+  };
 
-    const deleteUser = async(id_user) => {
-        try {
-            const response = await axios.delete(`https://usermanagement-api.vercel.app/api/delete-user/${id_user}`);
+  const deleteUser = async (id_user) => {
+    try {
+      const response = await axios.delete(
+        `https://usermanagement-api.vercel.app/api/delete-user/${id_user}`
+      );
 
-            setMessage(response.data.message);
-            setShowModal(true);
+      setMessage(response.data.message);
+      setShowModal(true);
 
-            setTimeout(() => {
-                setShowModal(false);
-                navigate('/user-list')
-            });
+      setTimeout(() => {
+        setShowModal(false);
+        navigate("/user-list");
+      });
 
-            // refresh the data
-            getUsers();
-            
-        } catch(error) {
-            console.error("Error : ", error);
-        }
+      // refresh the data
+      getUsers();
+    } catch (error) {
+      console.error("Error : ", error);
     }
+  };
 
-    useEffect(() => {
-        getUsers()
-    }, []);
+  useEffect(() => {
+    getUsers();
+  }, []);
 
-
-    return (
-        <div className="flex h-screen">
-            <div className="flex flex-1 flex-col">
-                <NavBar/>
-                <div className="container mx-auto mt-7 sm:px-6 lg:px-8">
-                    <h1 className="text-2xl font-serif font-bold mb-3">List User</h1>
-                    <h2>
-                        <Link to={'/add-user'} className="bg-blue-500 inline-block px-4 py-2 text-white rounded-lg shadow-lg hover:bg-blue-700 mb-3">
-                            Tambah user
+  return (
+    <div className="flex h-screen">
+      <div className="flex flex-1 flex-col">
+        <NavBar />
+        <div className="container mx-auto mt-7 sm:px-6 lg:px-8">
+          <h1 className="text-2xl font-serif font-bold mb-3">List User</h1>
+          <h2>
+            <Link
+              to={"/add-user"}
+              className="bg-blue-500 inline-block px-4 py-2 text-white rounded-lg shadow-lg hover:bg-blue-700 mb-3"
+            >
+              Tambah user
+            </Link>
+          </h2>
+          <div className="overflow-x-auto mt-3">
+            <Card>
+              <Table className="animate-slide-down min-w-[800px]">
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="font-medium font-sans text-sm sm:text-base lg:text-[16px] px-4 py-2">
+                      Nama
+                    </TableHead>
+                    <TableHead className="font-medium font-sans text-sm sm:text-base lg:text-[16px] px-4 py-2">
+                      Email
+                    </TableHead>
+                    <TableHead className="font-medium font-sans text-sm sm:text-base lg:text-[16px] px-4 py-2">
+                      Nomor Telepon
+                    </TableHead>
+                    <TableHead className="font-medium font-sans text-sm sm:text-base lg:text-[16px] px-4 py-2">
+                      Status Aktif
+                    </TableHead>
+                    <TableHead className="font-medium font-sans text-sm sm:text-base lg:text-[16px] px-4 py-2">
+                      Departemen
+                    </TableHead>
+                    <TableHead className="font-medium font-sans text-sm sm:text-base lg:text-[16px] px-4 py-2">
+                      Aksi
+                    </TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {users.map((user) => (
+                    <TableRow key={user.id}>
+                      <TableCell className="font-medium font-sans text-[16px] px-4 py-2">
+                        {user.nama}
+                      </TableCell>
+                      <TableCell className="font-medium font-sans text-[16px] px-4 py-2">
+                        {user.email}
+                      </TableCell>
+                      <TableCell className="font-medium font-sans text-[16px] px-4 py-2">
+                        {user.nomorTelepon}
+                      </TableCell>
+                      <TableCell className="font-medium font-sans text-[16px] px-4 py-2">
+                        {user.statusAktif.toString()}
+                      </TableCell>
+                      <TableCell className="font-medium font-sans text-[16px] px-4 py-2">
+                        {user.departemen}
+                      </TableCell>
+                      <TableCell className="space-x-2 gap-2">
+                        <Link
+                          to={`/update-user/${user.id}`}
+                          className="bg-slate-400 inline-block text-white px-4 py-2 rounded-lg shadow-lg hover:bg-slate-600"
+                        >
+                          Edit
                         </Link>
-                    </h2>
-                    <div className="overflow-x-auto mt-3">
-                        <Card>
-                            <Table className="animate-slide-down min-w-[800px]">
-                                <TableHeader>
-                                    <TableRow>
-                                        <TableHead className="font-medium font-sans text-sm sm:text-base lg:text-[16px] px-4 py-2">Nama</TableHead>
-                                        <TableHead className="font-medium font-sans text-sm sm:text-base lg:text-[16px] px-4 py-2">Email</TableHead>
-                                        <TableHead className="font-medium font-sans text-sm sm:text-base lg:text-[16px] px-4 py-2">Nomor Telepon</TableHead>
-                                        <TableHead className="font-medium font-sans text-sm sm:text-base lg:text-[16px] px-4 py-2">Status Aktif</TableHead>
-                                        <TableHead className="font-medium font-sans text-sm sm:text-base lg:text-[16px] px-4 py-2">Departemen</TableHead>
-                                        <TableHead className="font-medium font-sans text-sm sm:text-base lg:text-[16px] px-4 py-2">Aksi</TableHead>
-                                    </TableRow>
-                                </TableHeader>
-                                <TableBody>
-                                    {users.map((user) => (
-                                        <TableRow key={user.id}>
-                                            <TableCell className="font-medium font-sans text-[16px] px-4 py-2">{user.nama}</TableCell>
-                                            <TableCell className="font-medium font-sans text-[16px] px-4 py-2">{user.email}</TableCell>
-                                            <TableCell className="font-medium font-sans text-[16px] px-4 py-2">{user.nomorTelepon}</TableCell>
-                                            <TableCell className="font-medium font-sans text-[16px] px-4 py-2">{user.statusAktif.toString()}</TableCell>
-                                            <TableCell className="font-medium font-sans text-[16px] px-4 py-2">{user.departemen}</TableCell>
-                                            <TableCell className="space-x-2 gap-2">
-                                                <Link to={`/update-user/${user.id}`} className="bg-slate-400 inline-block text-white px-4 py-2 rounded-lg shadow-lg hover:bg-slate-600">
-                                                    Edit
-                                                </Link>
-                                                <button className="bg-red-500 inline-block px-4 py-2 rounded-lg shadow-lg text-white hover:bg-red-700" onClick={() => deleteUser(user.id)}>
-                                                    Delete
-                                                </button>
-                                            </TableCell>
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </Card>
-                    </div>
-                </div>
-                <Modal
-                    isOpen={showModal}
-                    message={message}
-                    onClose={() => setShowModal(false)}
-                />
-            </div>
+                        <button
+                          className="bg-red-500 inline-block px-4 py-2 rounded-lg shadow-lg text-white hover:bg-red-700"
+                          onClick={() => deleteUser(user.id)}
+                        >
+                          Delete
+                        </button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Card>
+          </div>
         </div>
-    );
+        <Modal
+          isOpen={showModal}
+          message={message}
+          onClose={() => setShowModal(false)}
+        />
+      </div>
+    </div>
+  );
 }
--- src/dashboard/User/TambahUser/TambahUser.jsx
+++ src/dashboard/User/TambahUser/TambahUser.jsx
@@ -1,5 +1,11 @@
 import NavBar from "@/components/Navbar";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import axios from "axios";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
@@ -7,140 +13,173 @@ import Modal from "@/components/Modal";
 import { Button } from "@/components/ui/button";
 
 export default function TambahUserPages() {
-    const [nama, setNama] = useState("");
-    const [email, setEmail] = useState("");
-    const [noTelp, setNoTelp] = useState("");
-    const [statusAktif, setStatusAktif] = useState("");
-    const [departemen, setDepartemen] = useState("");
-    const [message, setMessage] = useState("");
-    const [showModal, setShowModal] = useState(false);
-    const navigate = useNavigate();
+  const [nama, setNama] = useState("");
+  const [email, setEmail] = useState("");
+  const [noTelp, setNoTelp] = useState("");
+  const [statusAktif, setStatusAktif] = useState("");
+  const [departemen, setDepartemen] = useState("");
+  const [message, setMessage] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
 
-    const addUser = async(e) => {
-        e.preventDefault()
-        try {
-            const response = await axios.post("https://usermanagement-api.vercel.app/api/add-users", {
-                nama: nama,
-                email: email,
-                nomorTelepon: noTelp,
-                statusAktif: statusAktif,
-                departemen: departemen
-            });
-
-            setMessage(response.data.message);
-            setShowModal(true);
-
-            setTimeout(() => {
-                setShowModal(false);
-                navigate('/users');
-            }, 2000);
-
-            // clear the form
-            setNama("");
-            setEmail("");
-            setNoTelp("");
-            setStatusAktif("");
-            setDepartemen("");
-        } catch(error) {
-            console.error("Error : ", error);
-
-            setMessage("Error! Tidak dapat menambah user baru!");
-            setShowModal(true);
+  const addUser = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://usermanagement-api.vercel.app/api/add-user",
+        {
+          nama: nama,
+          email: email,
+          nomorTelepon: noTelp,
+          statusAktif: statusAktif,
+          departemen: departemen,
         }
-    }
+      );
 
-    return (
-        <div className="flex h-screen">
-            <div className="flex flex-1 flex-col">
-                <NavBar/>
-                <div className="container mx-auto mt-7 sm:px-6 lg:px-8">
-                    <div className="mx-auto max-w mt-10">
-                        <Card>
-                            <CardContent>
-                                <CardHeader>
-                                    <CardTitle className="text-2xl font-medium font-sans">
-                                        Form tambah User
-                                    </CardTitle>
-                                </CardHeader>
-                                <form onSubmit={addUser} method="POST" className="space-y-6 animate-slide-down">
-                                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 p-6">
-                                        <div>
-                                            <label htmlFor="nama" className="block text-sm font-medium font-sans text-gray-700 text-[16px]">Nama</label>
-                                            <input 
-                                                type="text" 
-                                                id="nama" 
-                                                name="nama"
-                                                onChange={(e) => setNama(e.target.value)}
-                                                className="mt-1 w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" 
-                                            />
-                                        </div>
-                                        <div>
-                                            <label htmlFor="email" className="block text-sm font-medium font-sans text-gray-700 text-[16px]">Email</label>
-                                            <input 
-                                                type="email" 
-                                                id="email" 
-                                                name="email"
-                                                onChange={(e) => setEmail(e.target.value)}
-                                                className="mt-1 w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" 
-                                            />
-                                        </div>
-                                        <div>
-                                            <label htmlFor="nomortelepon" className="block text-sm font-medium font-sans text-gray-700 text-[16px]">Nomor Telepon</label>
-                                            <input 
-                                                type="text" 
-                                                id="nomortelepon" 
-                                                name="nomortelepon"
-                                                onChange={(e) => setNoTelp(e.target.value)}
-                                                className="mt-1 w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" 
-                                            />
-                                        </div>
-                                        <div>
-                                            <label htmlFor="departemen" className="block text-sm font-medium font-sans text-gray-700 text-[16px]">Departemen</label>
-                                            <input 
-                                                type="text" 
-                                                id="departemen" 
-                                                name="departemen"
-                                                onChange={(e) => setDepartemen(e.target.value)}
-                                                className="mt-1 w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" 
-                                            />
-                                        </div>
-                                        <div>
-                                            <label htmlFor="statusAktif" className="block text-sm font-medium font-sans text-gray-700 text-[16px]">Status Aktif</label>
-                                            <select 
-                                                id="statusAktif"
-                                                name="statusAktif"
-                                                onChange={(e) => setStatusAktif(e.target.value)}
-                                                className="mt-1 w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                            >
-                                                <option value="">Pilih status</option>
-                                                <option value="true">Aktif</option>
-                                                <option value="false">Tidak Aktif</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div className="flex items-center gap-4 px-6 pb-6">
-                                        <button type="submit" className="bg-blue-600 text-white px-4 py-2 font-sans rounded-lg hover:bg-blue-700 transition">
-                                            Tambah
-                                        </button>
-                                    </div>
-                                </form>
-                            </CardContent>
-                            <CardFooter>
-                                <Button variant={'secondary'}>
-                                    <Link to={'/user-list'}>
-                                        Kembali
-                                    </Link>
-                                </Button>
-                            </CardFooter>
-                        </Card>
+      setMessage(response.data.message);
+      setShowModal(true);
+
+      setTimeout(() => {
+        setShowModal(false);
+        navigate("/user-list");
+      }, 2000);
+
+      // clear the form
+      setNama("");
+      setEmail("");
+      setNoTelp("");
+      setStatusAktif("");
+      setDepartemen("");
+    } catch (error) {
+      console.error("Error : ", error);
+
+      setMessage("Error! Tidak dapat menambah user baru!");
+      setShowModal(true);
+    }
+  };
+
+  return (
+    <div className="flex h-screen">
+      <div className="flex flex-1 flex-col">
+        <NavBar />
+        <div className="container mx-auto mt-7 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w mt-10">
+            <Card>
+              <CardContent>
+                <CardHeader>
+                  <CardTitle className="text-2xl font-medium font-sans">
+                    Form tambah User
+                  </CardTitle>
+                </CardHeader>
+                <form
+                  onSubmit={addUser}
+                  method="POST"
+                  className="space-y-6 animate-slide-down"
+                >
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-6 p-6">
+                    <div>
+                      <label
+                        htmlFor="nama"
+                        className="block text-sm font-medium font-sans text-gray-700 text-[16px]"
+                      >
+                        Nama
+                      </label>
+                      <input
+                        type="text"
+                        id="nama"
+                        name="nama"
+                        onChange={(e) => setNama(e.target.value)}
+                        className="mt-1 w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                      />
                     </div>
-                </div>
-            </div>
-            <Modal>
-                isOpen={showModal} 
-                message={message} 
-                onClose={() => setShowModal(false)}
-            </Modal>
+                    <div>
+                      <label
+                        htmlFor="email"
+                        className="block text-sm font-medium font-sans text-gray-700 text-[16px]"
+                      >
+                        Email
+                      </label>
+                      <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="mt-1 w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                      />
+                    </div>
+                    <div>
+                      <label
+                        htmlFor="nomortelepon"
+                        className="block text-sm font-medium font-sans text-gray-700 text-[16px]"
+                      >
+                        Nomor Telepon
+                      </label>
+                      <input
+                        type="text"
+                        id="nomortelepon"
+                        name="nomortelepon"
+                        onChange={(e) => setNoTelp(e.target.value)}
+                        className="mt-1 w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                      />
+                    </div>
+                    <div>
+                      <label
+                        htmlFor="departemen"
+                        className="block text-sm font-medium font-sans text-gray-700 text-[16px]"
+                      >
+                        Departemen
+                      </label>
+                      <input
+                        type="text"
+                        id="departemen"
+                        name="departemen"
+                        onChange={(e) => setDepartemen(e.target.value)}
+                        className="mt-1 w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                      />
+                    </div>
+                    <div>
+                      <label
+                        htmlFor="statusAktif"
+                        className="block text-sm font-medium font-sans text-gray-700 text-[16px]"
+                      >
+                        Status Aktif
+                      </label>
+                      <select
+                        id="statusAktif"
+                        name="statusAktif"
+                        onChange={(e) => setStatusAktif(e.target.value)}
+                        className="mt-1 w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                      >
+                        <option value="">Pilih status</option>
+                        <option value="true">Aktif</option>
+                        <option value="false">Tidak Aktif</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="flex items-center gap-4 px-6 pb-6">
+                    <button
+                      type="submit"
+                      className="bg-blue-600 text-white px-4 py-2 font-sans rounded-lg hover:bg-blue-700 transition"
+                    >
+                      Tambah
+                    </button>
+                  </div>
+                </form>
+              </CardContent>
+              <CardFooter>
+                <Button variant={"secondary"}>
+                  <Link to={"/user-list"}>Kembali</Link>
+                </Button>
+              </CardFooter>
+            </Card>
+          </div>
         </div>
-    );
+      </div>
+      <Modal>
+        isOpen={showModal}
+        message={message}
+        onClose={() => setShowModal(false)}
+      </Modal>
+    </div>
+  );
 }
